4312e8951b21f91ec1bea736e4c86a8bafe3aa08
jimfugit <1024719708@qq.com>
2021-04-26 14:29:51 +0800

    删除测试文件test.txt和删除文件夹userdata_test,增加RF331E文件夹

---
+++ RF331E/技术文档/bk3432/sdk/SDK/del.bat
@@ -0,0 +1,19 @@
+del Untitled*.*  /s/q
+del ..\readme.docx  /s/q
+rd projects\ble_stack /s /q
+rd projects\ble_stack_1 /s /q
+rd projects\rf_test /s /q
+rd sdk\ble_stack\src /s /q
+rd projects\ble_app_ancs\obj /s /q
+rd projects\ble_app_gatt\obj /s /q
+rd projects\ble_app_gatt_1\obj /s /q
+rd projects\ble_app_gatt_128\obj /s /q
+rd projects\ble_app_gatt_long_pdu_advertising\obj /s /q
+rd projects\ble_app_gatt_muti_chars\obj /s /q
+rd projects\ble_app_gatt_scan_advertising\obj /s /q
+rd projects\ble_app_gma_1\obj /s /q
+rd projects\ble_app_remoteobj /s /q
+rd projects\ble_app_wechat\obj /s /q
+del ..\..\bk3432_V*.rar  /s/q
+
+
